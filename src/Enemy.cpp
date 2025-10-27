bd9f27fbead78a3b913d9ef496934ef034a1eea7
Aasta-EB <286852@stud.inn.no>
2025-10-27 13:53:14 +0100

    Enemy movement

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -2,9 +2,41 @@
 
 void Enemy::EnemyMovement()
 {
+	if (GetRandomValue(0, 100) < 5)
+	{
+		enemyDirection = GetRandomValue(0, 3);
+
+		switch (enemyDirection)
+		{
+			// Venstre
+		case 0:
+			enemyPosition.x -= enemySpeed * GetFrameTime();
+			break;
+
+			//Høyre
+		case 1:
+			enemyPosition.x += enemySpeed * GetFrameTime();
+			break;
+
+			// Opp
+		case 2:
+			enemyPosition.y -= enemySpeed * GetFrameTime();
+			break;
+
+			// Ned
+		case 3:
+			enemyPosition.y += enemySpeed * GetFrameTime();
+			break;
+
+		default: 
+			break;
+
+		}
+		
+	}
 }
 
 void Enemy::DrawEnemy()
 {
-
+	DrawCircle(enemyPosition.x, enemyPosition.y, enemySize, enemyColor);
 }
