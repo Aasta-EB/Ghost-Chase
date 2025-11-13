2b6b71621ff47a192e947584172e11cce57239a9
Aasta-EB <286852@stud.inn.no>
2025-11-13 12:32:50 +0100

    Working on the enemy movement
    The enemy moves, but has no collision...

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -1,11 +1,117 @@
 #include "Enemy.h"
 
+void Enemy::CollisionCheck()
+{
+	int enemyPositionX = position.x / 50;
+	int enemyPositionY = position.y / 50;
+
+	if (enemyDirection.x == 1)
+	{
+		enemyPositionX = (position.x + 50) / 50;
+		enemyPositionY = position.y / 50;
+
+		if (map.map[enemyPositionY][enemyPositionX] == 1)
+		{
+			collisionRight = true;
+		}
+		else
+		{
+			collisionRight = false;
+		}
+	}
+	if (enemyDirection.x == -1)
+	{
+		enemyPositionX = (position.x - 50) / 50;
+		enemyPositionY = position.y / 50;
+		if (map.map[enemyPositionY][enemyPositionX] == 1)
+		{
+			collisionLeft = true;
+		}
+		else
+		{
+			collisionLeft = false;
+		}
+	}
+	if (enemyDirection.y == -1)
+	{
+		enemyPositionX = position.x / 50;
+		enemyPositionY = (position.y - 50) / 50;
+		if (map.map[enemyPositionY][enemyPositionX] == 1)
+		{
+			collisionUp = true;
+		}
+		else
+		{
+			collisionUp = false;
+		}
+	}
+	if (enemyDirection.y == 1)
+	{
+		enemyPositionX = position.x / 50;
+		enemyPositionY = (position.y + 50) / 50;
+		if (map.map[enemyPositionY][enemyPositionX] == 1)
+		{
+			collisionDown = true;
+		}
+		else
+		{
+			collisionDown = false;
+		}
+	}
+}
+
+
 void Enemy::EnemyMovement()
 {
+	for (int i = 0; i < counterTail; i++) enemyPosition = position;
 
+	if ((framesCounter % 10) == 0)
+	{
+		for (int i = 0; i < counterTail; i++)
+		{
+			if (i == 0)
+			{
+				CollisionCheck();
+				if (collisionRight == false && collisionLeft == false && collisionUp == false && collisionDown == false)
+				{
+					position.x += speed.x;
+					position.y += speed.y;
+				}
+				else if (enemyDirection.x == 1 && collisionRight == false)
+				{
+					position.x += speed.x;
+					position.y += speed.y;
+				}
+				else if (enemyDirection.x == -1 && collisionLeft == false)
+				{
+					position.x += speed.x;
+					position.y += speed.y;
+				}
+				else if (enemyDirection.y == -1 && collisionUp == false)
+				{
+					position.x += speed.x;
+					position.y += speed.y;
+				}
+				else if (enemyDirection.y == 1 && collisionDown == false)
+				{
+					position.x += speed.x;
+					position.y += speed.y;
+				}
+				position.x += speed.x;
+				position.y += speed.y;
+
+			}
+			else
+			{
+				position = enemyPosition;
+			}
+		}
+	}
+	framesCounter++;
 }
 
 void Enemy::DrawEnemy()
 {
+	EnemyMovement();
 	for (int i = 0; i < 1; i++) DrawRectangleV(position, size, color);
 }
--- src/Enemy.h
+++ src/Enemy.h
@@ -1,13 +1,30 @@
 #pragma once
 #include "raylib.h"
+#include "Grid.h"
 
 class Enemy
 {
 public: 
+	
+	Grid map;
 
 	Vector2 position = { 1400 / 2,700 / 2 };
 	Vector2 size = { 50,50 };
 	Color color = YELLOW;
+	Vector2 speed = { map.boxSize, 0 };
+
+	Vector2 enemyPosition = { 0 };
+	Vector2 enemyDirection = { 1,0 };
+
+	int counterTail = 1; 
+	int framesCounter = 0;
+
+	bool collisionLeft = false;
+	bool collisionRight = true;
+	bool collisionUp = false;
+	bool collisionDown = false;
+	
+	void CollisionCheck();
 
 	void EnemyMovement();
 
