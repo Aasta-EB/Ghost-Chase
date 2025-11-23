f455ef4222b44f9c5ea0fc051d3379a3e2ea1667
Aasta-EB <286852@stud.inn.no>
2025-11-23 12:36:36 +0100

    Commenting out unused math functions

--- src/Vector2d.cpp
+++ src/Vector2d.cpp
@@ -35,7 +35,7 @@ float Vector2d::CalculateDeltatoTarget(Vector2d inVectorTarget)
 
 Vector2d Vector2d::SumVectors(Vector2d inOtherVector)
 {
-	Vector2d summedVectors = { x + inOtherVector.x, y + inOtherVector.y }; 
+	Vector2d summedVectors = { x + inOtherVector.x, y + inOtherVector.y };
 
 	return summedVectors;
 }
@@ -80,16 +80,12 @@ Vector2d Vector2d::CalculateCosineWave(float inAmplitude, float inFrequency, flo
 	return cosineWaveVector;
 }
 
-
-// Code from class, NEEDS CHANGING
-// Sets the offset to the vector
-Vector2d Vector2d::OffsetVector(Vector2d inVectorToAdd)
-{
-	float outVectorX = x + inVectorToAdd.x;
-	float outVectorY = y + inVectorToAdd.y;
-	return { outVectorX, outVectorY };
-
-}
+//Vector2d Vector2d::OffsetVector(Vector2d inVectorToAdd)
+//{
+//	float outVectorX = x + inVectorToAdd.x;
+//	float outVectorY = y + inVectorToAdd.y;
+//	return { outVectorX, outVectorY };
+//}
 
 // Scales the vector
 Vector2d Vector2d::ScaleVector(float scale)
@@ -98,7 +94,6 @@ Vector2d Vector2d::ScaleVector(float scale)
 	float scaleVectorY = y * scale;
 
 	return { scaleVectorX, scaleVectorY };
-
 }
 
 Vector2d Vector2d::NormalizeVector()
@@ -109,68 +104,68 @@ Vector2d Vector2d::NormalizeVector()
 	}
 
 	// normalizing a the vector by taking its value divided by its magnitude(length)
-	float normalizedXVector = x / CalculateDeltaVector();
-	float normalizedYVector = y / CalculateDeltaVector();
-	Vector2d normalizedVector{ normalizedXVector,normalizedYVector };
+	float normalizedVectorX = x / CalculateDeltaVector();
+	float normalizedVectorY = y / CalculateDeltaVector();
+	Vector2d normalizedVector{ normalizedVectorX,normalizedVectorY };
 
 	return normalizedVector;
 }
 // Uses cosine to get movement
-Vector2d Vector2d::CosineMovement(Vector2d inCenter, float inAmplitude, float inAngle)
-{
-	float x = inCenter.x + inAmplitude * cosf(inAngle);
-	float y = inCenter.y;
-
-	return { x, y };
-}
-
-// Creates a circular motion
-Vector2d Vector2d::CircularMotion(Vector2d inCenter, float inRadius, float inAngle)
-{
-	float cosX = inCenter.x + inRadius * cosf(inAngle);
-	float sinY = inCenter.y + inRadius * sinf(inAngle);
-
-	return { cosX, sinY };
-}
+//Vector2d Vector2d::CosineMovement(Vector2d inCenter, float inAmplitude, float inAngle)
+//{
+//	float x = inCenter.x + inAmplitude * cosf(inAngle);
+//	float y = inCenter.y;
+//
+//	return { x, y };
+//}
+//
+//// Creates a circular motion
+//Vector2d Vector2d::CircularMotion(Vector2d inCenter, float inRadius, float inAngle)
+//{
+//	float cosX = inCenter.x + inRadius * cosf(inAngle);
+//	float sinY = inCenter.y + inRadius * sinf(inAngle);
+//
+//	return { cosX, sinY };
+//}
 
 // Finding the dot product
-float Vector2d::DotProduct(Vector2d inOtherVector)
-{
-	float xComponentMultiplied = x * inOtherVector.x;
-	float yComponentMultiplied = y * inOtherVector.y;
-
-	float dotProduct = xComponentMultiplied + yComponentMultiplied;
-
-	return 	dotProduct;
-}
+//float Vector2d::DotProduct(Vector2d inOtherVector)
+//{
+//	float xComponentMultiplied = x * inOtherVector.x;
+//	float yComponentMultiplied = y * inOtherVector.y;
+//
+//	float dotProduct = xComponentMultiplied + yComponentMultiplied;
+//
+//	return 	dotProduct;
+//}
 
 // Finding the angle between two vectors
-float Vector2d::AngleBetweenVectors(Vector2d inOtherVector)
-{
-	float thisLenght = CalculateDeltaVector();
-	float otherLenght = inOtherVector.CalculateDeltaVector();
+//float Vector2d::AngleBetweenVectors(Vector2d inOtherVector)
+//{
+//	float thisLenght = CalculateDeltaVector();
+//	float otherLenght = inOtherVector.CalculateDeltaVector();
+//
+//	if (thisLenght == 0 || otherLenght == 0)
+//	{
+//		return 0.0f;
+//	}
+//
+//	float dotProduct = DotProduct(inOtherVector);
+//
+//	float cosine = dotProduct / (thisLenght * otherLenght);
+//
+//	float degrees = acosf(cosine) * (180 / 3.14);
+//	// Tells the program to find the cosine then multiplying it so it is shown in degrees and not radians
+//
+//	return degrees;
+//}
 
-	if (thisLenght == 0 || otherLenght == 0)
-	{
-		return 0.0f;
-	}
-
-	float dotProduct = DotProduct(inOtherVector);
-
-	float cosine = dotProduct / (thisLenght * otherLenght);
-
-	float degrees = acosf(cosine) * (180 / 3.14);
-	// Tells the program to find the cosine then multiplying it so it is shown in degrees and not radians
-
-	return degrees;
-}
-
-float Vector2d::CrossProduct(Vector2d inOtherVector)
-{
-	float aComponentMultiplied = x * inOtherVector.y;
-	float bComponentMultibplied = y * inOtherVector.x;
-
-	float crossProduct = aComponentMultiplied - bComponentMultibplied;
-
-	return crossProduct;
-}
+//float Vector2d::CrossProduct(Vector2d inOtherVector)
+//{
+//	float aComponentMultiplied = x * inOtherVector.y;
+//	float bComponentMultibplied = y * inOtherVector.x;
+//
+//	float crossProduct = aComponentMultiplied - bComponentMultibplied;
+//
+//	return crossProduct;
+//}
