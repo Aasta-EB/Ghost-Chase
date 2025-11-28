04612f651bf2d3d9241d901e225633b30cbbba0e
Aasta-EB <286852@stud.inn.no>
2025-11-28 17:28:19 +0100

    Added an enemy hint

--- src/Enemy.h
+++ src/Enemy.h
@@ -33,10 +33,12 @@ public:
 	bool enemyHintExsists_2 = false;
 	bool enemyHintExsists_3 = false;
 	bool enemyHintExsists_4 = false;
+	bool enemyHintExsists_5 = false;
 	Vector2d enemyHintPos_1 = { 50, 50 };
 	Vector2d enemyHintPos_2 = { 50, 50 };
 	Vector2d enemyHintPos_3 = { 50, 50 };
 	Vector2d enemyHintPos_4 = { 50, 50 };
+	Vector2d enemyHintPos_5 = { 50, 50 };
 
 	// Enemy functions
 	void DrawDropHint();
